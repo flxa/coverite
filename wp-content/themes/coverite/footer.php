b95b2821c60f2eeb0e6d3f1223f5ec63edf97f12
Nicholas Fletcher <nick.flxa@gmail.com>
2014-11-13 02:44:50 +1100

    finally

---
+++ wp-content/themes/coverite/footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the "site-content" div and all content after.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+?>
+
+	</div><!-- .site-content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">Integrity – Reliability – Range – Attention – Quality</div>
+		<div class="site-info">
+			<a href="index.php">&copy; <?php echo date('Y'); ?> Coverite</a>
+		</div><!-- .site-info -->
+	</footer><!-- .site-footer -->
+</div><!-- .site -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ wp-content/themes/coverite/header-home.php
@@ -0,0 +1,7 @@
+<header class="template-header">
+
+	<?php #if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('homeWidgets') ) : ?>
+		
+	<?php #endif; ?>
+	
+</header>
